6488b796fc415ba2d5191db686d093406b3bb70e
jarqprog <jarqprog@gmail.com>
2017-08-28 08:58:56 +0200

    add inventory update function and fix some problems with display hero chart

--- game_szbJ.py
+++ game_szbJ.py
@@ -1,5 +1,6 @@
 # Szlakiem Bawarii - plik roboczy Jarka
-import os, time, random  
+import os, time, random
+#from msvcrt import getch
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -47,13 +48,16 @@ class Inv_items:
 def hero_setting(): # BOHATER - inicjacja zmiennych ############################## 
     attrib_regular_dict = {"atak":3,"obrona":2, "zwinność":1, "percepcja":1, "inteligencja":1, "siła woli":2}
     attrib_additional_dict = {"życie":13, "pełne życie":30, "doświadczenie":24, "poziom doświadczenia":1,"doświadczenie":12, "pkt do następnego poziomu": 30, "złoto":2 }
-    wolek_zbozowy = {u"name":"strzała","value":18,"info":"pocisk do łuku","price":1,"weight":2}
-    i_e_d = {} # inventory empty dict (pusty słownik przedmiotu w inventory) = używany, jeśli pozycja w inventory jest pusta:
-    # i# - pozycja w inventory
+
+    ################## items will go to another function (like enemies)
     placek_sliwkowy = {u"name":"placek śliwkowy","related":"jedzenie","value":2,"info":"zjedz mnie","cena":2,"waga":1}
     wolek_zbozowy = {u"name":"wołek zbożowy","related":"robak","value":10,"info":"jestem szkodnikiem","cena":0,"waga":0}   
-    inventory_dict = {u"i1":placek_sliwkowy,"i2":wolek_zbozowy,"i3":i_e_d,"i4":i_e_d,"i5":i_e_d,"i6":i_e_d,"i7":i_e_d,"i8":i_e_d,"i9":i_e_d,"i10":i_e_d,"i11":i_e_d,"i12":i_e_d,"i13":i_e_d,"i14":i_e_d,"i15":i_e_d,"i16":i_e_d,"i17":i_e_d,"i18":i_e_d,"i19":i_e_d,"i20":i_e_d}
+
+    ################## inventory_dict keep only names and values of items (without deep specyfication)
+    inventory_dict = {"nóż":1,"placki":10, "resztki mapy":1} #placki będzie można sprzedać albo nakarmić głodnego (quest)
+
     ################################ show active items on Hero:
+
     onbody_dict = {u'głowa':'skórzany hełm','szyja':'','tors':'skórzany kaftan','lewa ręka':'','prawa ręka':'maczuga','palec':'','kieszeń':'sok z gumijagód'}
     ################################ przykładowy bohater (odpalenie funkcji hero_crea )
     player_character = Hero("Rabarbar","Zbójcerz", attrib_regular_dict, attrib_additional_dict, inventory_dict, onbody_dict)
@@ -76,6 +80,7 @@ def enemy_settings(name = None, loc = None, lvl = None, gen = None):
         genre (gen, ex. gen = "animal": it will choose between enemies with genre = "animal")
         then export to main 
     '''
+
     attrib_dict = {} # pusty słownik dla atrybutów przeciwników
     treasure_dict = {} # pusty słownik z miejscem na przedmioty u przeciwników (można to wykorzystać m.in. do "losowania łupu")
     specials_list = [] # pusty słownik z miejscem na cechy specjalne (możemy je wyświetlać w trakcie walki) - domyślnie pusta
@@ -141,8 +146,7 @@ def enemy_settings(name = None, loc = None, lvl = None, gen = None):
     enemies_all_list = [wolf, goblin, skurczybyk, bear, bear_special_quest, troll, mountain_giant]
 
 
-# if enemy name was specified, it will export enemy by given name:
-
+    # if enemy name was specified, it will export enemy by given name:
     enemy_exported_to_main = ''
     if name != None:
         
@@ -152,8 +156,10 @@ def enemy_settings(name = None, loc = None, lvl = None, gen = None):
 
         return enemy_exported_to_main
 
+
+    # if enemy name wasn't specified, it will export random enemy (random using optional filters - level, location, genre):
     else:
-        enemy_random_list = []
+        enemy_random_list = [] # temporary helper list
         for element in enemies_all_list:        
             if (loc in element.location or loc == None) and (lvl == element.level or lvl == None) and (gen == element.genre or gen == None):
                 enemy_random_list.append(element)
@@ -168,28 +174,6 @@ def enemy_settings(name = None, loc = None, lvl = None, gen = None):
 
     #   # name, life, level, attrib_dict, treasure_dict, genre, xp4hero, specials, speach, location
     #wolf = Enemy("wilk",10, 1, attrib_dict,treasure_dict,"animal",5,None,"Wrrrrr!",
-
-def enemy_random_choice(Enemy, game_lvl = None):  # PRZECIWNICY - inicjacja zmiennych ##############################
-
-    attrib_dict = {} # pusty słownik dla atrybutów przeciwników
-    treasure_dict = {} # pusty słownik z miejscem na przedmioty u przeciwników (można to wykorzystać m.in. do "losowania łupu")
-    # (self, name, life, dmg, attrib_dict, treasure_dict, genre, xp4hero, specials, speach, location)
-    wolf = Enemy("wilk",10, 3, attrib_dict,treasure_dict,"animal",5,None,"Wrrrrr!",4)
-    wolf.attrib_dict = {"atak":3,"obrona":2, "zwinność":1, "percepcja":1, "inteligencja":1, "siła woli":2, "obrażenia":[1,3]}  
-    #dog = Enemy("pies",1,1,1,2,"animal",5,1,None,"Hau!")
-    #bear = Enemy("niedźwiedź",2,1,3,30,"animal",15,2,None,"Mrrrrrruummm!")
-    troll = Enemy("troll",80,[3,6],attrib_dict,treasure_dict,"beast",15,"Kamienna skóra - trolla ciężko ubić","Co Ty chcieć?!")
-    troll.attrib_dict = {"atak":3,"obrona":2, "zwinność":1, "percepcja":1, "inteligencja":1, "siła woli":2, "obrażenia":[1,3]}
-    troll.treasure_dict = {"złoto":(random.randint(1,100)), "rubiny":(random.randint(0,12))}
-
-    if game_lvl == 2:
-        return troll
-    elif game_lvl == 1:
-        return wolf
-    else:
-        return dog
-
-
 
 def intro():
     """display title, player choose between new game / load game / credits / HOF"""
@@ -268,22 +252,36 @@ def prints_enemy_attributes(Enemy = None): # dla testów
     Enemy = troll
     print(Enemy.speach, Enemy.specials)
 
+
+def inventory_update(player_character, add_remove_items_dict):
+    '''
+    add or remove items from hero's inventory
+    '''
+
+    player_character.inventory_dict.update(add_remove_items_dict)
+
+    temp_dict = {} # temporary inventory dict
+    for item in player_character.inventory_dict:
+        # if inventory key value is < 1 (empty position), remove item from inventory:
+        if player_character.inventory_dict[item] > 0:
+            temp_dict.update({item:player_character.inventory_dict[item]})
+    
+    player_character.inventory_dict = temp_dict
+
+    return player_character.inventory_dict
+    
+
 def display_hero_chart(player_character): #### todo
     """wyświetla kartę bohatera"""
     # przekształca słownniki z atrybutami bohatera na listy kluczy i wartości:
     attrib_regular_key_list = list(player_character.attrib_regular_dict.keys())
     attrib_regular_value_list = list(player_character.attrib_regular_dict.values())
-    attrib_additional_key_list = list(player_character.attrib_additional_dict.keys())
-    attrib_additional_value_list = list(player_character.attrib_additional_dict.values())
+
     #################### Ekwipunek do wyświetlenia ############################
-    i_e_d = {}
-    inventory_key_list = []
-    inventory_value_list = []
-    for item in player_character.inventory_dict:
-        if player_character.inventory_dict[item] != i_e_d:
-            inventory_key_list.append(player_character.inventory_dict[item]["name"])
-            inventory_value_list.append(player_character.inventory_dict[item]["value"])
-    
+    # player_character.inventory_dict
+    inventory_key_list = list(player_character.inventory_dict.keys())
+    inventory_value_list = list(player_character.inventory_dict.values())
+
     # przedmioty noszone na sobie
     # przekształcenie kluczy i wartości słownka w listy
     onbody_key_list = list(player_character.onbody_dict.keys())
@@ -294,27 +292,41 @@ def display_hero_chart(player_character): #### todo
     # (pomoże to odpowiednio wydrukować tabelkę):
     total_number_of_items = max(len(attrib_regular_key_list),len(inventory_key_list),len(onbody_key_list))
     # sprawdza najdłuższy wyraz wśród list atrybutów/umiejętności/ekwipunku/rzeczy na sobie - tworzy zmienne użyte przy druku tabeli:
-    attrib_regular_longest_arg = (max(len(x) for x in attrib_regular_key_list))+5
-    inventory_longest_arg = (max(len(x) for x in inventory_key_list))+5
-    onbody_key_longest_arg = (max(len(x) for x in onbody_key_list))+5
-    onbody_value_longest_arg = (max(len(x) for x in onbody_value_list))+5
-    sum_longest_arg = int(attrib_regular_longest_arg+inventory_longest_arg+onbody_key_longest_arg+onbody_value_longest_arg)
+    printing_var = 0 #helps correctly print hero chart
+    attrib_regular_longest_arg = (max(len(x) for x in attrib_regular_key_list))+printing_var
+    inventory_key_list.append("") # add to avoid error with "max" below
+    inventory_value_list.append("") # add to avoid error with "max" below
+    inventory_longest_key_arg = (max(len(x) for x in inventory_key_list))+printing_var
+    inventory_longest_value_arg = (max(len(str(x)) for x in inventory_value_list))
+    inventory_key_list.remove("") # remove - it's no longer needed
+    inventory_value_list.remove("") # remove - it's no longer needed
+    onbody_value_longest_arg = (max(len(x) for x in onbody_value_list))+printing_var
+    onbody_key_longest_arg = (max(len(x) for x in onbody_key_list))+printing_var
+    onbody_value_longest_arg = (max(len(x) for x in onbody_value_list))+printing_var
+    sum_longest_arg = int(attrib_regular_longest_arg+inventory_longest_key_arg+inventory_longest_value_arg+onbody_key_longest_arg+onbody_value_longest_arg)
     # ta część kodu rysuje tabelę:
     # zmienne do nagłówka: attrib_additional_dict = ,"doświadczenie":24, "poziom doświadczenia":1, "złoto":2 }
-    h00 = player_character.name.ljust(10)
-    h01 = player_character.proffession.ljust(10)
+    printing_var = 10 #helps correctly print hero chart
+    h00 = player_character.name.ljust(printing_var)
+    h01 = player_character.proffession.ljust(printing_var)
     h02 = player_character.attrib_additional_dict["poziom doświadczenia"]
     h03 = player_character.attrib_additional_dict["życie"]
     h04 = player_character.attrib_additional_dict["pełne życie"]
     h05 = player_character.attrib_additional_dict["doświadczenie"]
-    h06 = player_character.attrib_additional_dict["pkt do następnego poziomu"] 
-    h1 = 'atrybuty:'.rjust(attrib_regular_longest_arg+5)
-    h2 = 'w torbie:'.rjust(inventory_longest_arg+10)
-    h3 = 'na sobie:'.rjust(onbody_key_longest_arg+9)
+    h06 = player_character.attrib_additional_dict["pkt do następnego poziomu"]
+    printing_var = 5 #helps correctly print hero chart
+    h1 = 'atrybuty:'.rjust(attrib_regular_longest_arg+printing_var)
+    printing_var = 10 #helps correctly print hero chart
+    h2 = 'w torbie:'.rjust(inventory_longest_key_arg+printing_var)
+    printing_var = 5 #helps correctly print hero chart
+    h3 = 'na sobie:'.rjust(inventory_longest_value_arg+onbody_key_longest_arg+printing_var)
     print(u'\n\n','Karta postaci:\n\n',"imię:",h00,"klasa:",h01,'\tżycie:',h02,"/",h03,"\tdoświadczenie:",h05,"/",h06,'\n')
     print(h1,h2,h3)
-    print("-"*(sum_longest_arg+30))
+    printing_var_head_bottom = 25 #helps correctly print hero chart
+    print("-"*(sum_longest_arg+printing_var_head_bottom))
     for i in range(int(total_number_of_items)) :
+        
+        # printing attributes (atak, obrona...):
         try:
             int(len(attrib_regular_key_list)) >= i         
             if int(len(attrib_regular_key_list)) >= i:
@@ -324,15 +336,6 @@ def display_hero_chart(player_character): #### todo
         except:
             attribKeyPrt = ""
             attribValuePrt = ""
-        try: #zmień nazwy!!!
-            int(len(attrib_additional_key_list)) >= i         
-            if int(len(attrib_additional_key_list)) >= i:
-                iskills = int(i)
-                skillsKeyPrt = str(attrib_additional_key_list[iskills])
-                skillsValuePrt = str(attrib_additional_value_list[iskills])
-        except:
-            skillsKeyPrt = ""
-            skillsValuePrt = ""
         try:
             int(len(inventory_key_list)) >= i         
             if int(len(inventory_key_list)) >= i:
@@ -355,51 +358,64 @@ def display_hero_chart(player_character): #### todo
         #longest_arg = 10
         p1 = attribKeyPrt.rjust(attrib_regular_longest_arg)
         p2 = attribValuePrt.ljust(4)
-        p3 = '' #tmp
-        p4 = skillsValuePrt.ljust(4)
-        p5 = inventoryKeyPrt.rjust(inventory_longest_arg)
-        p6 = inventoryValuePrt.ljust(4)
-        p7 = onbodyKeyPrt.rjust(onbody_key_longest_arg)
-        p8 = onbodyValuePrt.ljust(onbody_value_longest_arg)
+        p3 = inventoryKeyPrt.rjust(inventory_longest_key_arg)
+        p4 = inventoryValuePrt.ljust(inventory_longest_value_arg)
+        p5 = onbodyKeyPrt.rjust(onbody_key_longest_arg)
+        p6 = onbodyValuePrt.ljust(onbody_value_longest_arg)
     # wnętrze tabeli
-        print(u'| ',p1,u':',p2,u'| ',p5,u':',p6,u'|',p7,u':',p8,u'|')
+        print(u'| ',p1,u':',p2,u'| ',p3,u':',p4,u'|',p5,u':',p6,u'|')
     # stopka tabeli
-    print("-"*(sum_longest_arg+30)) # TMP!! (magic number)
+    print("-"*(sum_longest_arg+printing_var_head_bottom))
     print("\nLokacja: Kto tu wejdzie, ten niechybnie zginie","\n...","\n")
+
+'''
+def map_maker():
+
+    with open('mapa.txt', 'r') as myfile:
+        mapa = myfile.read()
+    mapa = list(mapa)
+    position_horizontal = 1
+    position_vertical = 13
+    lenght_of_the_map_plus_one = 61
+    map_copy = mapa[:]
+    map_copy[position_horizontal + position_vertical * 61] = "@"
+    print("".join(map_copy))
+    while True:
+        print("Press w, s, d or a:")
+        input_char = getch()
+        print(input_char)
+        if input_char == b'w': 
+            map_copy[position_horizontal + position_vertical * 61] = "."
+            position_vertical -= 1
+            map_copy[position_horizontal + position_vertical * 61] = "@"
+            print("".join(map_copy))
+        elif input_char == b's': 
+            map_copy[position_horizontal + position_vertical * 61] = "."
+            position_vertical += 1
+            map_copy[position_horizontal + position_vertical * 61] = "@"
+            print("".join(map_copy))
+        elif input_char == b'd': 
+            map_copy[position_horizontal + position_vertical * 61] = "."
+            position_horizontal += 1
+            map_copy[position_horizontal + position_vertical * 61] = "@"
+            print("".join(map_copy))
+        elif input_char == b'a': 
+            map_copy[position_horizontal + position_vertical * 61] = "."
+            position_horizontal -= 1
+            map_copy[position_horizontal + position_vertical * 61] = "@"
+            print("".join(map_copy))
+        else:
+            continue
+
+'''
 
 def main():
     # ustawia nam podstawowe zmienne:
     game_lvl = 0
+    add_remove_items_dict = {}
     # importuje nam Postać do głównej funkcji (w procesie tworzenie postaci lub ładowawnia gry nastapi update postaci):
     player_character = hero_setting()
-    '''
-    ############ ustawiamy przeciwników - możemy tu ich definiować, ale chyba damy do osobnej funkcji...
-    wolf = Enemy("wilk",1,3,1,10,"animal",5,1,None,"Wrrrrr!")
-    dog = Enemy("pies",1,1,1,2,"animal",5,1,None,"Hau!")
-    bear = Enemy("niedźwiedź",2,1,3,30,"animal",15,2,None,"Mrrrrrruummm!")
-    troll = Enemy("troll",2,1,3,30,"beast",15,2,"Kamienna skóra - trolla ciężko ubić","Co Ty chcieć?!")
-    '''
     game_lvl = 1
-    imported_enemy = enemy_settings(name = "Zły Miś", loc = None, lvl = None, gen = None)
-    print(imported_enemy.name, imported_enemy.attrib_dict["obrażenia"], imported_enemy.attrib_dict["atak"], imported_enemy.treasure_dict.items())
-
-    # imported_enemy_rnd = imported random enemy from function 'enemy_settings':
-    imported_enemy_rnd = enemy_settings(name = None, loc = 1, lvl = 2, gen = "human")
-    print(imported_enemy_rnd.name, imported_enemy_rnd.speach_list, imported_enemy_rnd.treasure_dict)
-
-    #print(imported_enemy.name, imported_enemy.treasure_dict)
-    #print(imported_enemy.__dict__)
-    
-
-    #print(imported_enemy.treasure_dict, imported_enemy.dmg, imported_enemy.attrib_dict["atak"], imported_enemy.location)
-
-    #print(wolf)
-        
-    #enemy_random = enemy_random_choice(game_lvl)
-    #print(enemy_random.name, enemy_random.specials, enemy_random.treasure_dict)
-    #print(enemy_random.attributes())
-
-    #print(troll.name)
 
 ################ TU URUCHAMIAMY FUNKCJE:
     #intro() # tytuł, powitanie, wybór:
@@ -408,8 +424,27 @@ def main():
     ####### po każdej z tych funkcji odpalamy główną pętlę:
      
     # ----- tu będzie funkcja do wyświetlania mapy
-    #display_hero_chart(player_character)
-    #prints_enemy_attributes(troll(Enemy))
+
+    ''' testujemy'''
+
+    display_hero_chart(player_character)
+    # bawimy się modyfikacją inventory:
+    add_remove_items_dict = {"placek":1,"jacek":1}
+    player_character.inventory_dict = inventory_update(player_character, add_remove_items_dict)
+    add_remove_items_dict = {"placki":-11,"jacek":1}
+    player_character.inventory_dict = inventory_update(player_character, add_remove_items_dict)
+    display_hero_chart(player_character)
+    add_remove_items_dict = {"jacek":-10, "nóż":3, "placek":2}
+    player_character.inventory_dict = inventory_update(player_character, add_remove_items_dict)
+    display_hero_chart(player_character)
+    #losujemy przeciwnika (random), filtr ustawiony na przeciwników o levelu 4: 
+    imported_enemy_rnd = enemy_settings(name = None, loc = None, lvl = 4, gen = None)
+    #dodajemy jego inventory do inventory bohatera:
+    add_remove_items_dict = imported_enemy_rnd.treasure_dict
+    player_character.inventory_dict = inventory_update(player_character, add_remove_items_dict)
+    #wyświetlamy:
+    display_hero_chart(player_character)
+    #map_maker()
 
 
 
