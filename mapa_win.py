fc827dd98c24307cae214a298b9087fb76cce7b9
LMalko <30960917+LMalko@users.noreply.github.com>
2017-08-29 19:23:51 +0200

    zmieniłem parametry

--- mapa_win.py
+++ mapa_win.py
@@ -3,34 +3,34 @@ with open('mapa.txt', 'r') as myfile:
     mapa = myfile.read()
 mapa = list(mapa)
 position_horizontal = 1
-position_vertical = 13
-lenght_of_the_map_plus_one = 61
+position_vertical = 18
+lenght_of_the_map_plus_one = 81
 map_copy = mapa[:]
-map_copy[position_horizontal + position_vertical * 61] = "@"
+map_copy[position_horizontal + position_vertical * 81] = "@"
 print("".join(map_copy))
 while True:
     print("Press w, s, d or a:")
     input_char = getch()
     print(input_char)
     if input_char == b'w': 
-        map_copy[position_horizontal + position_vertical * 61] = "."
+        map_copy[position_horizontal + position_vertical * 81] = "."
         position_vertical -= 1
-        map_copy[position_horizontal + position_vertical * 61] = "@"
+        map_copy[position_horizontal + position_vertical * 81] = "@"
         print("".join(map_copy))
     elif input_char == b's': 
-        map_copy[position_horizontal + position_vertical * 61] = "."
+        map_copy[position_horizontal + position_vertical * 81] = "."
         position_vertical += 1
-        map_copy[position_horizontal + position_vertical * 61] = "@"
+        map_copy[position_horizontal + position_vertical * 81] = "@"
         print("".join(map_copy))
     elif input_char == b'd': 
-        map_copy[position_horizontal + position_vertical * 61] = "."
+        map_copy[position_horizontal + position_vertical * 81] = "."
         position_horizontal += 1
-        map_copy[position_horizontal + position_vertical * 61] = "@"
+        map_copy[position_horizontal + position_vertical * 81] = "@"
         print("".join(map_copy))
     elif input_char == b'a': 
-        map_copy[position_horizontal + position_vertical * 61] = "."
+        map_copy[position_horizontal + position_vertical * 81] = "."
         position_horizontal -= 1
-        map_copy[position_horizontal + position_vertical * 61] = "@"
+        map_copy[position_horizontal + position_vertical * 81] = "@"
         print("".join(map_copy))
     else:
         continue
