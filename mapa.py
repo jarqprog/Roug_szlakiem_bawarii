d7d505b036c384f6785b3670d56c6f0254c2cb38
LMalko <30960917+LMalko@users.noreply.github.com>
2017-08-29 20:10:50 +0200

    POPRAWIONE PORUSZANIE SIĘ PLUS KOLIZJE

--- mapa.py
+++ mapa.py
@@ -18,31 +18,48 @@ position_vertical = 18
 lenght_of_the_map_plus_one = 81
 map_copy = mapa[:]
 map_copy[position_horizontal + position_vertical * 81] = "@"
-print("".join(map_copy))
 while True:
-    print("Press w, s, d or a:")
+    print("".join(map_copy))
+    print("Press w, s, d or a to move, e for inventory, z for journal, p for help, g to save game, l to quit:")
     input_char = getch()
-    print(input_char)
     if input_char == 'w': 
-        map_copy[position_horizontal + position_vertical * 81] = "."
-        position_vertical -= 1
-        map_copy[position_horizontal + position_vertical * 81] = "@"
-        print("".join(map_copy))
+        if map_copy[position_horizontal + (position_vertical - 1) * 81] != ".":
+            print("nie mozesz sie tu ruszyc")
+        else:
+            map_copy[position_horizontal + position_vertical * 81] = "."
+            position_vertical -= 1
+            map_copy[position_horizontal + position_vertical * 81] = "@"
     elif input_char == 's': 
-        map_copy[position_horizontal + position_vertical * 81] = "."
-        position_vertical += 1
-        map_copy[position_horizontal + position_vertical * 81] = "@"
-        print("".join(map_copy))
-    elif input_char == 'd': 
-        map_copy[position_horizontal + position_vertical * 81] = "."
-        position_horizontal += 1
-        map_copy[position_horizontal + position_vertical * 81] = "@"
-        print("".join(map_copy))
-    elif input_char == 'a': 
-        map_copy[position_horizontal + position_vertical * 81] = "."
-        position_horizontal -= 1
-        map_copy[position_horizontal + position_vertical * 81] = "@"
-        print("".join(map_copy))
+        if map_copy[position_horizontal + (position_vertical + 1) * 81] != ".":
+            print("nie mozesz sie tu ruszyc")
+        else:
+            map_copy[position_horizontal + position_vertical * 81] = "."
+            position_vertical += 1
+            map_copy[position_horizontal + position_vertical * 81] = "@"
+    elif input_char == 'd':
+        if map_copy[(position_horizontal + 1) + position_vertical * 81] != ".":
+            print("nie mozesz sie tu ruszyc")
+        else:
+            map_copy[position_horizontal + position_vertical * 81] = "."
+            position_horizontal += 1
+            map_copy[position_horizontal + position_vertical * 81] = "@"
+    elif input_char == 'a':
+        if map_copy[(position_horizontal - 1) + position_vertical * 81] != ".":
+            print("nie mozesz sie tu ruszyc")
+        else:
+            map_copy[position_horizontal + position_vertical * 81] = "."
+            position_horizontal -= 1
+            map_copy[position_horizontal + position_vertical * 81] = "@"
+    elif input_char == 'e':
+        print("tu powinien być ekwipunek")
+    elif input_char == 'z':
+        print("tu powinien być dziennik")
+    elif input_char == 'p':
+        print("tu powinien być pomoc")
+    elif input_char == 'g':
+        print("tu powinien być zapis gry")
+    elif input_char == 'l':
+        break
     else:
         continue
 
--- mapa_win.py
+++ mapa_win.py
@@ -7,31 +7,48 @@ position_vertical = 18
 lenght_of_the_map_plus_one = 81
 map_copy = mapa[:]
 map_copy[position_horizontal + position_vertical * 81] = "@"
-print("".join(map_copy))
 while True:
-    print("Press w, s, d or a:")
+    print("".join(map_copy))
+    print("Press w, s, d or a to move, e for inventory, z for journal, p for help, g to save game, l to quit:")
     input_char = getch()
-    print(input_char)
     if input_char == b'w': 
-        map_copy[position_horizontal + position_vertical * 81] = "."
-        position_vertical -= 1
-        map_copy[position_horizontal + position_vertical * 81] = "@"
-        print("".join(map_copy))
+        if map_copy[position_horizontal + (position_vertical - 1) * 81] != ".":
+            print("nie mozesz sie tu ruszyc")
+        else:
+            map_copy[position_horizontal + position_vertical * 81] = "."
+            position_vertical -= 1
+            map_copy[position_horizontal + position_vertical * 81] = "@"
     elif input_char == b's': 
-        map_copy[position_horizontal + position_vertical * 81] = "."
-        position_vertical += 1
-        map_copy[position_horizontal + position_vertical * 81] = "@"
-        print("".join(map_copy))
-    elif input_char == b'd': 
-        map_copy[position_horizontal + position_vertical * 81] = "."
-        position_horizontal += 1
-        map_copy[position_horizontal + position_vertical * 81] = "@"
-        print("".join(map_copy))
-    elif input_char == b'a': 
-        map_copy[position_horizontal + position_vertical * 81] = "."
-        position_horizontal -= 1
-        map_copy[position_horizontal + position_vertical * 81] = "@"
-        print("".join(map_copy))
+        if map_copy[position_horizontal + (position_vertical + 1) * 81] != ".":
+            print("nie mozesz sie tu ruszyc")
+        else:
+            map_copy[position_horizontal + position_vertical * 81] = "."
+            position_vertical += 1
+            map_copy[position_horizontal + position_vertical * 81] = "@"
+    elif input_char == b'd':
+        if map_copy[(position_horizontal + 1) + position_vertical * 81] != ".":
+            print("nie mozesz sie tu ruszyc")
+        else:
+            map_copy[position_horizontal + position_vertical * 81] = "."
+            position_horizontal += 1
+            map_copy[position_horizontal + position_vertical * 81] = "@"
+    elif input_char == b'a':
+        if map_copy[(position_horizontal - 1) + position_vertical * 81] != ".":
+            print("nie mozesz sie tu ruszyc")
+        else:
+            map_copy[position_horizontal + position_vertical * 81] = "."
+            position_horizontal -= 1
+            map_copy[position_horizontal + position_vertical * 81] = "@"
+    elif input_char == b'e':
+        print("tu powinien być ekwipunek")
+    elif input_char == b'z':
+        print("tu powinien być dziennik")
+    elif input_char == b'p':
+        print("tu powinien być pomoc")
+    elif input_char == b'g':
+        print("tu powinien być zapis gry")
+    elif input_char == b'l':
+        break
     else:
         continue
 
