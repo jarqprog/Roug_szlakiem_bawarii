8ab6944fb90217beb5d7d599a0054fcdc2e88a58
LMalko <30960917+LMalko@users.noreply.github.com>
2017-08-30 11:57:31 +0200

    kolejne zmiany

--- mapa.py
+++ mapa.py
@@ -2,8 +2,12 @@ import os
 os.system('clear')
 # Zawsze odstep 10 wierszy !!!
 print("\n" * 10)
+
+
 def getch():
-    import sys, tty, termios
+    import sys
+    import tty
+    import termios
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
@@ -14,80 +18,126 @@ def getch():
     return ch
 
 
-with open('mapa_forest.txt', 'r') as myfile:
-    mapa = myfile.read()
-mapa = list(mapa)
-position_horizontal = 1
-position_vertical = 18
-lenght_of_the_map_plus_one = 81
-map_copy = mapa[:]
-map_copy[position_horizontal + position_vertical * 81] = "@"
-while True:
-    print("".join(map_copy))
-    print("Press w, s, d or a to move, e for inventory, z for journal, p for help, g to save game, l to quit:")
-    print("\n/ __||_  /| |     /_\  | |/ /|_ _|| __||  \/  | | _ )  /_\\ \    / //_\  | _ \|_ _||_ _|")
-    print("\__ \ / / | |__  / _ \ | ' <  | | | _| | |\/| | | _ \ / _ \\ \/\/ // _ \ |   / | |  | |")
-    print("|___//___||____|/_/ \_\|_|\_\|___||___||_|  |_| |___//_/ \_\\_/\_//_/ \_\|_|_\|___||___|")
-    input_char = getch()
-    if input_char == 'w': 
-        os.system('clear')
-        if map_copy[position_horizontal + (position_vertical - 1) * 81] != ".":
-            print("\n" * 9)
-            print("Nie mozesz sie tu ruszyc")
-        else:
-            print("\n" * 10)
-            map_copy[position_horizontal + position_vertical * 81] = "."
-            position_vertical -= 1
-            map_copy[position_horizontal + position_vertical * 81] = "@"
-    elif input_char == 's': 
-        os.system('clear')
-        if map_copy[position_horizontal + (position_vertical + 1) * 81] != ".":
-            print("\n" * 9)
-            print("Nie mozesz sie tu ruszyc")
-        else:
-            print("\n" * 10)
-            map_copy[position_horizontal + position_vertical * 81] = "."
-            position_vertical += 1
-            map_copy[position_horizontal + position_vertical * 81] = "@"
-    elif input_char == 'd':
-        os.system('clear')
-        if map_copy[(position_horizontal + 1) + position_vertical * 81] != ".":
-            print("\n" * 9)
-            print("Nie mozesz sie tu ruszyc")
-        else:
-            print("\n" * 10)
-            map_copy[position_horizontal + position_vertical * 81] = "."
-            position_horizontal += 1
-            map_copy[position_horizontal + position_vertical * 81] = "@"
-    elif input_char == 'a':
-        os.system('clear')
-        if map_copy[(position_horizontal - 1) + position_vertical * 81] != ".":
-            print("\n" * 9)
-            print("Nie mozesz sie tu ruszyc")
-        else:
-            print("\n" * 10)
-            map_copy[position_horizontal + position_vertical * 81] = "."
-            position_horizontal -= 1
-            map_copy[position_horizontal + position_vertical * 81] = "@"
-    elif input_char == 'e':
-        os.system('clear')
+def collision(position):
+    """ Returns message depending on what hero touches. """
+    if position == "N":
         print("\n" * 9)
-        print("Tu powinien być ekwipunek")
-    elif input_char == 'z':
-        os.system('clear')
+        print("Tutaj NPC powie ci co masz robić dalej.")
+    elif position == "W":
         print("\n" * 9)
-        print("Tu powinien być dziennik")
-    elif input_char == 'p':
-        os.system('clear')
+        print("Trafiłeś do wioski Szwarzwald")
+    elif position == "G":
         print("\n" * 9)
-        print("Tu powinien być pomoc")
-    elif input_char == 'g':
-        os.system('clear')
+        print("Trafiłeś do gaju Łotrzyków.")
+    elif position == "M":
         print("\n" * 9)
-        print("Tu powinien być zapis gry")
-    elif input_char == 'l':
-        os.system('clear')
-        break
+        print(" most pilnowany przez trolla Silnorękiego")
+    elif position == "+":
+        print("\n" * 9)
+        print("żródło życia")
+    elif position == "?":
+        print("\n" * 9)
+        print("Niespodzianka - wpadasz do rowu z kolcami zostawionego przez łotrzyków i giniesz.")
     else:
-        continue
+        print("\n" * 9)
+        print("Nie mozesz sie tu ruszyc")
 
+
+def core():
+    """ Main loop of the game."""
+    with open('mapa_forest.txt', 'r') as myfile:
+        mapa = myfile.read()
+    mapa = list(mapa)
+    position_horizontal = 1
+    position_vertical = 18
+    lenght_of_the_map_plus_one = 81
+    map_copy = mapa[:]
+    map_copy[position_horizontal + position_vertical * 81] = "@"
+    while True:
+        print("".join(map_copy))
+        print("\nWciśnij W, S, A, D - poruszanie się, 'e' - ekwipunek, 'z' - dziennik, 'p' - pomoc, ")
+        print("'g' - zapis gry, 'l' - legenda lub '0' - wyjście z gry.")
+        print("\n ___ _____      _   _  _____ ___ __  __        ___   ___      ___   ___ ___ ___ ")
+        print("/ __|_  / |    /_\ | |/ /_ _| __|  \/  |      | _ ) /_\ \    / /_\ | _ \_ _|_ _|")
+        print("\__ \/ /| |__ / _ \| ' < | || _|| |\/| |      | _ \/ _ \ \/\/ / _ \|   /| | | | ")
+        print("|___/___|____/_/ \_\_|\_\___|___|_|  |_|      |___/_/ \_\_/\_/_/ \_\_|_\___|___|")
+        input_char = getch()
+        if input_char.upper() == 'W':
+            os.system('clear')
+            if map_copy[position_horizontal + (position_vertical - 1) * 81] != ".":
+                collision(map_copy[position_horizontal + (position_vertical - 1) * 81])
+            else:
+                print("\n" * 10)
+                map_copy[position_horizontal + position_vertical * 81] = "."
+                position_vertical -= 1
+                map_copy[position_horizontal + position_vertical * 81] = "@"
+        elif input_char.upper() == 'S':
+            os.system('clear')
+            if map_copy[position_horizontal + (position_vertical + 1) * 81] != ".":
+                collision(map_copy[position_horizontal + (position_vertical + 1) * 81])
+            else:
+                print("\n" * 10)
+                map_copy[position_horizontal + position_vertical * 81] = "."
+                position_vertical += 1
+                map_copy[position_horizontal + position_vertical * 81] = "@"
+        elif input_char.upper() == 'D':
+            os.system('clear')
+            if map_copy[(position_horizontal + 1) + position_vertical * 81] != ".":
+                collision(map_copy[(position_horizontal + 1) + position_vertical * 81])
+            else:
+                print("\n" * 10)
+                map_copy[position_horizontal + position_vertical * 81] = "."
+                position_horizontal += 1
+                map_copy[position_horizontal + position_vertical * 81] = "@"
+        elif input_char.upper() == 'A':
+            os.system('clear')
+            if map_copy[(position_horizontal - 1) + position_vertical * 81] != ".":
+                collision(map_copy[(position_horizontal - 1) + position_vertical * 81])
+            else:
+                print("\n" * 10)
+                map_copy[position_horizontal + position_vertical * 81] = "."
+                position_horizontal -= 1
+                map_copy[position_horizontal + position_vertical * 81] = "@"
+        elif input_char.upper() == 'E':
+            os.system('clear')
+            print("\n" * 9)
+            print("Tu powinien być ekwipunek")
+        elif input_char.upper() == 'Z':
+            os.system('clear')
+            print("\n" * 9)
+            print("Tu powinien być dziennik")
+        elif input_char.upper() == 'P':
+            os.system('clear')
+            print("\n" * 9)
+            print("Tu powinien być pomoc")
+        elif input_char.upper() == 'G':
+            os.system('clear')
+            print("\n" * 9)
+            print("Tu powinien być zapis gry")
+        elif input_char.upper() == 'L':
+            os.system('cls')
+            print("\n" * 2)
+            print("Legenda:\n")
+            print("W = Wioska Szwarzwald")
+            print("? = Niespodzianka")
+            print("M = Most trolla Silnorękiego")
+            print("+ = Zródło życia")
+            print("G = Gaj Łotrzyków")
+            print("N = NPC")
+        elif input_char.upper() == '0':
+            os.system('cls')
+            print("\nNa pewno? Wciśnij jeszcze raz '0' żeby wyjść z gry, coś innego żeby kontynuować.")
+            input_char = getch()
+            if input_char.upper() == '0':
+                os.system('cls')
+                break
+            else:
+                os.system('cls')
+                print("\n" * 10)
+                continue
+        else:
+            os.system('cls')
+            print("\n" * 10)
+            continue
+
+core()
